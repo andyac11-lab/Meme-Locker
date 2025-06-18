36d9170a1301659a2ca2baae26948639d4380f60
andyac11-lab <andyeleven631@gmail.com>
2025-06-18 19:08:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-const adLink =https://www.profitableratecpm.com/z4mqz8div?key=64839d757daf4d8b6b640bd7992a37ca
-
 function showAdAndUnlock(memeElement) {
-    window.open(adLink, "_blank");
+  // Open the real Adsterra Direct Link in a new tab
+  window.open("https://www.profitableratecpm.com/z4mqz8div?key=64839d757daf4d8b6b640bd7992a37ca", "_blank");
 
-    setTimeout(() => {
-        memeElement.classList.remove("blurred");
-        memeElement.querySelector('.overlay').style.display = "none";
-    }, 5000); // Wait 5 seconds
-}
-function showAdAndUnlock(memeElement) {
-  // Simulate showing an ad
-  alert("Please wait... Watching Ad");
-
-  // Simulate a 5-second ad
+  // Simulate a 5-second ad before unlocking the meme
   setTimeout(() => {
     memeElement.classList.remove("blurred");
     alert("Ad finished! Meme unlocked.");
