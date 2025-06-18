631408926386c2d22c3f77abdf03d75b89b5b90b
andyac11-lab <andyeleven631@gmail.com>
2025-06-18 18:08:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-document.querySelectorAll('.meme').forEach((memeBox) => {
-  memeBox.addEventListener('click', () => {
-    alert('Ad will play here... (Insert Adsterra)');
-    memeBox.querySelector('img').classList.remove('blurred');
-    memeBox.querySelector('.unlock-overlay').style.display = 'none';
-  });
-});
+const adLink =https://www.profitableratecpm.com/z4mqz8div?key=64839d757daf4d8b6b640bd7992a37ca
+
+function showAdAndUnlock(memeElement) {
+    window.open(adLink, "_blank");
+
+    setTimeout(() => {
+        memeElement.classList.remove("blurred");
+        memeElement.querySelector('.overlay').style.display = "none";
+    }, 5000); // Wait 5 seconds
+}
